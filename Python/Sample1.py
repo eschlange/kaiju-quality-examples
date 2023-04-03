5aca725d6b6edcd52bb1c780c1ed042b60043166
Matthew Alexander <matthew.davidalexander@gmail.com>
2023-04-03 15:46:52 -0500

    Push additional changes I forgot to include in initial
    Adding additional changes I made and hilariously forgot to add to my commit because I am a dingus

--- Python/Sample1.py
+++ Python/Sample1.py
@@ -1,12 +1,29 @@
+"""This is the MOST basic sample of using Webdriver to validate a page is hit.
+Accesses selenium functions: get, find_element, send_keys, click, and find_element text
+Uses basic assert to provide "Pass/Fail status. try/except False assert to fail correctly"""
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://www.google.com/")
 
+driver.find_element(By.NAME, "q").send_keys("test pyramid")
+print("Input text: test pyramid")
+driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]").click()
+print("Click 'Google Search' button")
 
-driver.find_element(By.NAME, "q").send_keys("Butts")
+expected_text = "Images for test pyramids"
+actual_text = driver.find_element(By.XPATH,
+                                  "//*[@id='rso']/div[2]/div/div/div[2]/g-section-with-header/div[1]/title-with-lhs-icon/a/div[3]/h3").text
 
-time.sleep(5)
+if expected_text == actual_text:
+    print("Text matches! :)")
+    print("{0} == {1}".format(expected_text, actual_text))
+    assert True
+else:
+    try:
+        assert False
+    except AssertionError as err:
+        print("Text did not match. :(")
+        print("{0} != {1}".format(expected_text, actual_text))
